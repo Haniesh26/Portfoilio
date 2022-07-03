b4720d52b8ddf0dde4b5b1a02a63b03b319d4e20
Haniesh26 <hani0021@algonquinlive.com>
2022-07-03 18:32:35 -0400

    alter

--- script.js
+++ script.js
@@ -7,13 +7,12 @@ const progressBarPercents = [97, 90, 86, 85, 83, 65, 60]
 
 window.addEventListener('scroll', () => {
   if (window.pageYOffset >= navBarOffsetTop) {
-    console
     navbar.classList.add('sticky')
   } else {
     navbar.classList.remove('sticky')
   }
 
-  if (window.pageYOffset + window.innerHeight >= progress.offsetTop) {
+  if (window.pageYOffset + window.innerHeight <= progress.offsetTop) {
     const progressPercent = document.querySelectorAll('.progress-percent')
     console.log('progress')
     document.querySelectorAll('.progress-percent').forEach((element, i) => {
